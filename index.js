921524d1cf2974cc24ba4c6cc603d56478f52a5b
Rick Jensen <rick@linkin.se>
2016-07-04 22:23:52 +0200

    fixed prototype typos

--- index.js
+++ index.js
@@ -1,51 +1,53 @@
 var WPJS = {
-    base_url,
-    init: function(url) {
-        this.base_url = url;
-    },
-    call: function (domain, endpoint, args, method) {
-        if (method === null) {
-            method = 'GET';
-        }
-        var promise = new Promise(function(resolve, reject) {
-            var client = new XMLHttpRequest();
-            var uri = url;
-            if (args && (method === 'POST' || method === 'PUT')) {
-                uri += '?';
-                var argcount = 0;
-                for (var key in args) {
-                    if (args.hasOwnProperty(key)) {
-                        if (argcount++) {
-                            uri += '&';
-                        }
-                        uri += encodeURIComponent(key) + '=' + encodeURIComponent(args[key]);
-                    }
-                }
+        init: function(url) {
+            this.base_url = url;
+        },
+        call: function (domain, endpoint, args, method) {
+            if (typeof method !== 'string') {
+                method = 'GET';
             }
 
-            client.open(method, [this.base_url, domain, 'wp-json', endpoint].join('/'));
-            client.send();
-            client.onload = function () {
-                if (this.status >= 200 && this.status < 300) {
-                    resolve(this.response);
-                } else {
-                    reject(this.statusText);
+            var url = [this.base_url, 'wp-json', domain+'/v2', endpoint].join('/');
+
+            var promise = new Promise(function(resolve, reject) {
+                var client = new XMLHttpRequest();
+                var uri = url;
+                if (args && (method === 'POST' || method === 'PUT')) {
+                    uri += '?';
+                    var argcount = 0;
+                    for (var key in args) {
+                        if (args.hasOwnProperty(key)) {
+                            if (argcount++) {
+                                uri += '&';
+                            }
+                            uri += encodeURIComponent(key) + '=' + encodeURIComponent(args[key]);
+                        }
+                    }
                 }
-            };
-            client.onerror = function () {
-                reject(this.statusText);
-            };
-        });
-        return promise;
-    },
-    'Pages': {
-        'list': (args) => this.call('wp', 'pages', args),
-        'get': (id) => this.call('wp', 'pages', args)
-    },
-    'Posts': {
-        'list': (args) => this.call('wp', 'posts', args),
-        'get': (id) => this.call('wp', 'posts', args)
-    }
+
+                client.open(method, uri);
+                client.send();
+                client.onload = function () {
+                    if (this.status >= 200 && this.status < 300) {
+                        resolve(JSON.parse(this.response));
+                    } else {
+                        reject(this.statusText);
+                    }
+                };
+                client.onerror = function () {
+                    reject(this.statusText);
+                };
+            });
+            return promise;
+        },
+        'Pages': {
+            'list': (args) => WPJS.call('wp', 'pages', args),
+        'get': (id) => WPJS.call('wp', 'pages/'+id.toString())
+},
+'Posts': {
+    'list': (args) => WPJS.call('wp', 'posts', args),
+        'get': (id) => WPJS.call('wp', 'posts'+id.toString())
+}
 }
 module.exports = WPJS;
 
